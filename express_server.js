1e1d3ccf343438aee520c97d9379fc37a6844e60
Rance Campbell <rancewcampbell@gmail.com>
2020-04-13 21:21:18 +0000

    add views dir for templates and updated server.js accordingly

--- express_server.js
+++ express_server.js
@@ -17,10 +17,15 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req,res) => {
+app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
 app.listen(PORT, () => {
-console.log(`Exampe app listening on port ${PORT}!`);
+  console.log(`Exampe app listening on port ${PORT}!`);
 });
