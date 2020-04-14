b1caea5d395cbda10f6bf70c8ae84877293a02d2
Rance Campbell <rancewcampbell@gmail.com>
2020-04-14 21:03:46 +0000

    deleted useless route

--- express_server.js
+++ express_server.js
@@ -30,10 +30,6 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
