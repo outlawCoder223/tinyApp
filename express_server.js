07b0ba90a9b658d650b60e228c31826e3ffe1dfd
Rance Campbell <rancewcampbell@gmail.com>
2020-04-14 19:35:29 +0000

    fixed redirect on POST method when creating URL

--- express_server.js
+++ express_server.js
@@ -44,8 +44,8 @@ app.get('/urls/new', (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
 });
 
 app.post('/urls', (req, res) => {
