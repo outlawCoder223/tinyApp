928f27b3648b9d2e3bc3b0d52e845678424c53c8
Rance Campbell <rancewcampbell@gmail.com>
2020-04-14 22:33:21 +0000

    login redirects to /urls

--- express_server.js
+++ express_server.js
@@ -62,6 +62,10 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 });
 
+app.post('/login', (req, res) => {
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
