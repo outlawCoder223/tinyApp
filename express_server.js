9946c787cad2a1282046895c5b69ee5ef1e72ca0
Rance Campbell <rancewcampbell@gmail.com>
2020-04-16 21:19:05 +0000

    add /url router

--- express_server.js
+++ express_server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-const bcrypt = require('bcryptjs');
-const { generateUniqueString, checkEmail } = require('./helpers');
+// const bcrypt = require('bcryptjs');
+// const { generateUniqueString, checkEmail } = require('./helpers');
 const { urlDatabase, userDatabase } = require('./database');
 
 const PORT = 8080;
@@ -32,6 +32,7 @@ app.use((req, res, next) => {
   next();
 });
 
+const urlRouter = require('./routes/urls');
 const uRouter = require('./routes/u');
 const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
@@ -47,82 +48,84 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/urls', (req, res) => {
+app.use('/urls', urlRouter);
+
+// app.get('/urls', (req, res) => {
   
-  res.render('urls_index', req.templateVars);
-});
+//   res.render('urls_index', req.templateVars);
+// });
 
-app.post('/urls', (req, res) => {
-  if (req.templateVars.user) {
-    const shortURL = generateUniqueString(urlDatabase);
-    urlDatabase[shortURL] = {
-      shortURL,
-      longURL: req.body.longURL,
-      date: new Date().toDateString(),
-      userID: req.templateVars.user.id
-    };
-    res.redirect(`/urls/${shortURL}`);
-  } else {
-    req.templateVars.message = 'You do not have permission to do this.';
-    res.status(403).render('urls_error', req.templateVars);
-  }
+// app.post('/urls', (req, res) => {
+//   if (req.templateVars.user) {
+//     const shortURL = generateUniqueString(urlDatabase);
+//     urlDatabase[shortURL] = {
+//       shortURL,
+//       longURL: req.body.longURL,
+//       date: new Date().toDateString(),
+//       userID: req.templateVars.user.id
+//     };
+//     res.redirect(`/urls/${shortURL}`);
+//   } else {
+//     req.templateVars.message = 'You do not have permission to do this.';
+//     res.status(403).render('urls_error', req.templateVars);
+//   }
   
-});
+// });
 
-app.get('/urls/new', (req, res) => {
-  if (!req.templateVars.user) {
-    res.redirect('/login');
-  } else {
-    res.render('urls_new', req.templateVars);
-  }
-});
+// app.get('/urls/new', (req, res) => {
+//   if (!req.templateVars.user) {
+//     res.redirect('/login');
+//   } else {
+//     res.render('urls_new', req.templateVars);
+//   }
+// });
 
-app.get('/urls/:shortURL', (req, res) => {
-  const url = req.params.shortURL;
-  if (!urlDatabase[url]) {
-    req.templateVars.message = 'Not found';
-    res.status(404).render('urls_error.ejs', req.templateVars);
-  } else if (!req.templateVars.user) {
-    res.redirect('/login');
-  } else if (urlDatabase[url].userID === req.templateVars.user.id) {
-    const templateVars = {
-      ...req.templateVars,
-      shortURL: req.params.shortURL,
-      longURL: urlDatabase[req.params.shortURL].longURL
-    };
-    res.render('urls_show', templateVars);
-  } else {
-    req.templateVars.message = 'Access denied';
-    res.status(403).render('urls_error', req.templateVars);
-  }
+// app.get('/urls/:shortURL', (req, res) => {
+//   const url = req.params.shortURL;
+//   if (!urlDatabase[url]) {
+//     req.templateVars.message = 'Not found';
+//     res.status(404).render('urls_error.ejs', req.templateVars);
+//   } else if (!req.templateVars.user) {
+//     res.redirect('/login');
+//   } else if (urlDatabase[url].userID === req.templateVars.user.id) {
+//     const templateVars = {
+//       ...req.templateVars,
+//       shortURL: req.params.shortURL,
+//       longURL: urlDatabase[req.params.shortURL].longURL
+//     };
+//     res.render('urls_show', templateVars);
+//   } else {
+//     req.templateVars.message = 'Access denied';
+//     res.status(403).render('urls_error', req.templateVars);
+//   }
   
-});
+// });
 
-app.post('/urls/:shortURL', (req, res) => {
-  const url = req.params.shortURL;
-  if (!req.templateVars.user) {
-    res.redirect('/login');
-  } else if (urlDatabase[url].userID === req.templateVars.user.id) {
-    urlDatabase[req.params.shortURL].longURL = req.body.update;
-    res.redirect('/urls');
-  } else {
-    req.templateVars.message = 'You do not have permission to do this.';
-    res.status(403).render('urls_error', req.templateVars);
-  }
-});
+// app.post('/urls/:shortURL', (req, res) => {
+//   const url = req.params.shortURL;
+//   if (!req.templateVars.user) {
+//     res.redirect('/login');
+//   } else if (urlDatabase[url].userID === req.templateVars.user.id) {
+//     urlDatabase[req.params.shortURL].longURL = req.body.update;
+//     res.redirect('/urls');
+//   } else {
+//     req.templateVars.message = 'You do not have permission to do this.';
+//     res.status(403).render('urls_error', req.templateVars);
+//   }
+// });
 
-app.post('/urls/:shortURL/delete', (req, res) => {
-  const url = req.params.shortURL;
-  if (!req.templateVars.user) {
-    res.redirect('/login');
-  } else if (urlDatabase[url].userID === req.templateVars.user.id) {
-    delete urlDatabase[req.params.shortURL];
-    res.redirect('/urls');
-  } else {
-    req.templateVars.message = 'You do not have permission to do this.';
-    res.status(403).render('urls_error', req.templateVars);
-  }
-});
+// app.post('/urls/:shortURL/delete', (req, res) => {
+//   const url = req.params.shortURL;
+//   if (!req.templateVars.user) {
+//     res.redirect('/login');
+//   } else if (urlDatabase[url].userID === req.templateVars.user.id) {
+//     delete urlDatabase[req.params.shortURL];
+//     res.redirect('/urls');
+//   } else {
+//     req.templateVars.message = 'You do not have permission to do this.';
+//     res.status(403).render('urls_error', req.templateVars);
+//   }
+// });
 
 app.use('/u', uRouter);
 
---
+++ routes/urls.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const router = express.Router();
+const { generateUniqueString } = require('../helpers');
+const { urlDatabase, userDatabase } = require('../database');
+
+
+router.get('/', (req, res) => {
+  
+  res.render('urls_index', req.templateVars);
+});
+
+router.post('/', (req, res) => {
+  if (req.templateVars.user) {
+    const shortURL = generateUniqueString(urlDatabase);
+    urlDatabase[shortURL] = {
+      shortURL,
+      longURL: req.body.longURL,
+      date: new Date().toDateString(),
+      userID: req.templateVars.user.id
+    };
+    res.redirect(`/urls/${shortURL}`);
+  } else {
+    req.templateVars.message = 'You do not have permission to do this.';
+    res.status(403).render('urls_error', req.templateVars);
+  }
+  
+});
+
+router.get('/new', (req, res) => {
+  if (!req.templateVars.user) {
+    res.redirect('/login');
+  } else {
+    res.render('urls_new', req.templateVars);
+  }
+});
+
+router.get('/:shortURL', (req, res) => {
+  const url = req.params.shortURL;
+  if (!urlDatabase[url]) {
+    req.templateVars.message = 'Not found';
+    res.status(404).render('urls_error.ejs', req.templateVars);
+  } else if (!req.templateVars.user) {
+    res.redirect('/login');
+  } else if (urlDatabase[url].userID === req.templateVars.user.id) {
+    const templateVars = {
+      ...req.templateVars,
+      shortURL: req.params.shortURL,
+      longURL: urlDatabase[req.params.shortURL].longURL
+    };
+    res.render('urls_show', templateVars);
+  } else {
+    req.templateVars.message = 'Access denied';
+    res.status(403).render('urls_error', req.templateVars);
+  }
+  
+});
+
+router.post('/:shortURL', (req, res) => {
+  const url = req.params.shortURL;
+  if (!req.templateVars.user) {
+    res.redirect('/login');
+  } else if (urlDatabase[url].userID === req.templateVars.user.id) {
+    urlDatabase[req.params.shortURL].longURL = req.body.update;
+    res.redirect('/urls');
+  } else {
+    req.templateVars.message = 'You do not have permission to do this.';
+    res.status(403).render('urls_error', req.templateVars);
+  }
+});
+
+router.post('/:shortURL/delete', (req, res) => {
+  const url = req.params.shortURL;
+  if (!req.templateVars.user) {
+    res.redirect('/login');
+  } else if (urlDatabase[url].userID === req.templateVars.user.id) {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect('/urls');
+  } else {
+    req.templateVars.message = 'You do not have permission to do this.';
+    res.status(403).render('urls_error', req.templateVars);
+  }
+});
+
+module.exports = router;
