e10deadfce9e03623626e6b43b4abea0373b108c
Rance Campbell <rancewcampbell@gmail.com>
2020-04-16 15:59:00 +0000

    password is hashed and stored at signup

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
 const PORT = 8080;
 
 class User {
@@ -209,18 +210,19 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
   const id = generateUniqueString(userDatabase);
-  const { email, password } = req.body;
+  const hashedPass = bcrypt.hashSync(req.body.password, 10);
+  const email = req.body.email;
   if (!email) {
     req.templateVars.message = 'Enter a valid email';
     res.status(400).render('urls_error', req.templateVars)
-  } else if (!password) {
+  } else if (!hashedPass) {
     req.templateVars.message = 'Please enter a password.';
     res.status(400).render('urls_error', req.templateVars)
   } else if (checkEmail(userDatabase, email)) {
     req.templateVars.message = 'That email is already registered to a user!'
     res.status(400).render('urls_error', req.templateVars)
   } else {
-    userDatabase[id] = new User(id, email, password);
+    userDatabase[id] = new User(id, email, hashedPass);
     res.cookie('userID', id);
     res.redirect('/urls');
   }
